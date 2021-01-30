c3c55f450f49c5560ce26d8268a4eb98cf0cfc65
Shujaat Azim <shujaat.azim@gmail.com>
2021-01-30 15:58:46 -0500

    Added browser router functionality, organized with App just being an entry point with the switch statement. This separates concerns rather nicely.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Projects from './Pages/Projects'
+import Resume from './Pages/Resume'
+import { Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/resume" component={Resume} />
+    </Switch>
   )
 }
 

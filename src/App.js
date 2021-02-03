fd9b3ec6a6e2941c4089e0fe47b4e838d16567cc
Shujaat Azim <shujaat.azim@gmail.com>
2021-02-02 19:01:28 -0500

    Small changes, but installed and uninstalled material ui after trying it.

--- src/App.js
+++ src/App.js
@@ -8,17 +8,23 @@ import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
 
-  // Making app serve as an "entry" and only handle the routing as a separation of concerns. 
+  // Making app serve as an "entry" and only handle the routing as a separation of concerns.
   // Might add the navbar here though, so the same navbar is served for each page/component.
+  // This will require a whole separate div, but might work better than adding nav to every page.
 
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/projects" component={Projects} />
-      <Route path="/resume" component={Resume} />
-    </Switch>
+    <div>
+      <div>
+        
+      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/resume" component={Resume} />
+      </Switch>
+    </div>
   );
 }
 

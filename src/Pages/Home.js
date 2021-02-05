532dddced528b35849e4167d629ddfda7cbe28b8
Shujaat Azim <shujaat.azim@gmail.com>
2021-02-05 18:37:37 -0500

    Added more content to home page and added associated styles.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,14 +3,25 @@ import { Grid } from '@material-ui/core';
 
 const Home = () => {
   return (
-    <Grid container style={{ textAlign: "center" }}>
-      <Grid item xs={12}>
-        <img className="avatar" src="https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/7_avatar-512.png" alt="profile" />
+    <div className="homepage">
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <img className="avatar" src="https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/7_avatar-512.png" alt="profile" />
+        </Grid>
+        <Grid item xs={12}>
+          <div className="hero">
+            <h3>Full Stack Developer</h3>
+            <hr />
+            <p> React | Ruby on Rails | JavaScript | Ruby | HTML/CSS | NodeJS | RecoilJS </p>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className="banner">
+            Icons
+          </div>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <h3 className="hero">Full Stack Developer</h3>
-      </Grid>
-    </Grid>
+    </div>
   );
 }
 

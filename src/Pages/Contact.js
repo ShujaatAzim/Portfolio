85ddee784446e692fa8fdf198458b0f1f99176a4
Shujaat Azim <shujaat.azim@gmail.com>
2021-02-11 23:59:06 -0500

    Starting on contact page.

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -4,6 +4,8 @@ const Contact = () => {
   return (
     <div style={{ fontFamily: "Anton", height: "85vh" }}>
       <h1>Contact Page</h1>
+      <div>
+        This will be side one.</div>
     </div>
   );
 }

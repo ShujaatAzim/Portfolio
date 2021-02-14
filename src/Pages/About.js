f8c47744a102c371799cfc8ceed73785b7bb0748
Shujaat Azim <shujaat.azim@gmail.com>
2021-02-13 23:59:44 -0500

    Added grid container to about.

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,6 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div style={{ fontFamily: "Anton", height: "85vh" }}>
+      <Grid container></Grid>
       <h1>About Page</h1>
     </div>
   )

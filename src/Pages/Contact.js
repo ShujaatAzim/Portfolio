4460bb3414638f07d2e3a3455c5aa2299b6d0aa3
Shujaat Azim <shujaat.azim@gmail.com>
2021-02-13 23:58:54 -0500

    Added layout to grid in contact page.

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -5,7 +5,16 @@ const Contact = () => {
     <div style={{ fontFamily: "Anton", height: "85vh" }}>
       <h1>Contact Page</h1>
       <Grid container>
-        
+        <Grid item xs={6}>
+          <div>
+            Profile Pic
+          </div>
+        </Grid>
+        <Grid item xs={6}>
+          <div>
+            Other Stuff
+          </div>
+        </Grid>
       </Grid>
       <div>
         This will be side one.

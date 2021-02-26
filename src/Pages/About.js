aebb657b6c251be9b7e9722926611ec9d81af355
Shujaat Azim <shujaat.azim@gmail.com>
2021-02-25 23:44:20 -0500

    Started on About page.

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,6 +4,9 @@ const About = () => {
   return (
     <div className="about-page">
       <h1>About Page</h1>
+      <p>
+        This is going to be about me, my story, and what I plan to do.
+      </p>
     </div>
   )
 }

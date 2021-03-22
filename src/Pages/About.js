9de69e7ed7c487e7ccaaf9164662a6cefae3cfe7
Shujaat Azim <shujaat.azim@gmail.com>
2021-03-21 23:42:25 -0400

    Added grid to about page, setting up scaffold.

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 
 const About = () => {
   return (
     <div className="about-page">
-      <h1>About Page</h1>
-      <p>
-        This is going to be about me, my story, and what I plan to do.
-      </p>
-      <p>
-        0) Before software dev.
-        1) How I got started in dev.
-        2) Where I am now.
-        3) Where I want to be.
-      </p>
+      <Grid container>
+        <h1>About Page</h1>
+        <p>
+          This is going to be about me, my story, and what I plan to do.
+        </p>
+        <p>
+          0) Before software dev.
+          1) How I got started in dev.
+          2) Where I am now.
+          3) Where I want to be.
+        </p>
+      </Grid>
     </div>
   )
 }

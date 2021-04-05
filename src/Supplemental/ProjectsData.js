1ae3adeac6fcbdade8343d3bf937255728949900
Shujaat Azim <shujaat.azim@gmail.com>
2021-04-04 22:49:45 -0400

    Added project data fields for future filling.

--- src/Supplemental/ProjectsData.js
+++ src/Supplemental/ProjectsData.js
@@ -3,68 +3,80 @@ export const projectsData = {
     {
       "title": "Full Stack Project 1",
       "image": "https://static.thenounproject.com/png/390337-200.png",
-      "desc": "This is a description for sample full stack project 1, which will eventually be unique!"
+      "desc": "This is a description for sample full stack project 1, which will eventually be unique!",
+      "github": ""
     },
     {
       "title": "Full Stack Project 2",
       "image": "https://static.thenounproject.com/png/390337-200.png",
-      "desc": "This is a description for sample full stack project 2, which will eventually be unique!"
+      "desc": "This is a description for sample full stack project 2, which will eventually be unique!",
+      "github": ""
     },
     {
       "title": "Full Stack Project 3",
       "image": "https://static.thenounproject.com/png/390337-200.png",
-      "desc": "This is a description for sample full stack project 3, which will eventually be unique!"
+      "desc": "This is a description for sample full stack project 3, which will eventually be unique!",
+      "github": ""
     }
   ],
   "react": [
     {
       "title": "React Project 1",
       "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
-      "desc": "This is a description for sample react project 1, which will eventually be unique!"
+      "desc": "This is a description for sample react project 1, which will eventually be unique!",
+      "github": ""
     },
     {
       "title": "React Project 2",
       "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
-      "desc": "This is a description for sample react project 2, which will eventually be unique!"
+      "desc": "This is a description for sample react project 2, which will eventually be unique!",
+      "github": ""
     },
     {
       "title": "React Project 3",
       "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
-      "desc": "This is a description for sample react project 3, which will eventually be unique!"
+      "desc": "This is a description for sample react project 3, which will eventually be unique!",
+      "github": ""
     },
   ],
   "rails": [
     {
       "title": "Rails Project 1",
       "image": "https://www.yaya.today/img/referral/Technologies/tech_rubyonrails.png",
-      "desc": "This is a description for sample rails project 1, which will eventually be unique!"
+      "desc": "This is a description for sample rails project 1, which will eventually be unique!",
+      "github": ""
     },
     {
       "title": "Rails Project 2",
       "image": "https://www.yaya.today/img/referral/Technologies/tech_rubyonrails.png",
-      "desc": "This is a description for sample rails project 2, which will eventually be unique!"
+      "desc": "This is a description for sample rails project 2, which will eventually be unique!",
+      "github": ""
     },
     {
       "title": "Rails Project 3",
       "image": "https://www.yaya.today/img/referral/Technologies/tech_rubyonrails.png",
-      "desc": "This is a description for sample rails project 3, which will eventually be unique!"
+      "desc": "This is a description for sample rails project 3, which will eventually be unique!",
+      "github": ""
     },
   ],
   "misc": [
     {
       "title": "Miscellaneous Project 1",
       "image": "https://clubrunner.blob.core.windows.net/00000000127/Images/misc.png",
-      "desc": "This is a description for sample misc project 1, which will eventually be unique!"
+      "desc": "This is a description for sample misc project 1, which will eventually be unique!",
+      "github": ""
     },
     {
       "title": "Miscellaneous Project 2",
       "image": "https://clubrunner.blob.core.windows.net/00000000127/Images/misc.png",
-      "desc": "This is a description for sample misc project 2, which will eventually be unique!"
+      "desc": "This is a description for sample misc project 2, which will eventually be unique!",
+      "github": ""
     },
     {
       "title": "Miscellaneous Project 3",
       "image": "https://clubrunner.blob.core.windows.net/00000000127/Images/misc.png",
-      "desc": "This is a description for sample misc project 3, which will eventually be unique!"
+      "desc": "This is a description for sample misc project 3, which will eventually be unique!",
+      "github": ""
     }
   ]
 }

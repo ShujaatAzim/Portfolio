0cb3c8a206ddb16af532806bbc4781eda5b8e1dd
Shujaat Azim <shujaat.azim@gmail.com>
2021-06-07 19:17:35 -0400

    Made resume bar responsive. Might need to also do so for project tab bar, but so far it's okay.

--- src/Components/ResumeTabBar.js
+++ src/Components/ResumeTabBar.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles({
   root: {
@@ -12,7 +17,9 @@ const useStyles = makeStyles({
 
 const ResumeTabBar = props => {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const matches = useMediaQuery('(min-width:1000px)');
 
   const handleChange = (event, newValue) => {
     newValue === 0 ? props.setView("employment") : 
@@ -22,7 +29,27 @@ const ResumeTabBar = props => {
     setValue(newValue);
   };
 
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
+    !matches ?           
+    <React.Fragment>
+      <IconButton aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        <MenuIcon />
+      </IconButton>
+      <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+        <MenuItem onClick={() => {props.setView("employment");handleClose()}}>Employment History</MenuItem>
+        <MenuItem onClick={() => {props.setView("skills");handleClose()}}>Relevant Skills</MenuItem>
+        <MenuItem onClick={() => {props.setView("education");handleClose()}}>Education</MenuItem>
+        <MenuItem onClick={() => {props.setView("extras");handleClose()}}>Volunteerism/Hobbies</MenuItem>
+      </Menu>
+    </React.Fragment> :
     <Paper className={classes.root} elevation={0} style={{ backgroundColor: "transparent", borderRadius: "0px" }}>
       <Tabs value={value} onChange={handleChange} centered style={{ flexDirection: "row" }}>
         <Tab label="Employment History" />

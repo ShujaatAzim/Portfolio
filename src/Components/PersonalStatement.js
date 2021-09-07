df6125feacab30b3428d48e470e1221a293aef8c
Shujaat Azim <shujaat.azim@gmail.com>
2021-09-06 23:55:48 -0400

    Minor changes to personal statement and about page.

--- src/Components/PersonalStatement.js
+++ src/Components/PersonalStatement.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PersonalStatement = () => {
   return (
     <p>
-      I am an enthusiastic full stack software developer with a background in, and passion for technology, medicine, and finance. I have
+      I am an enthusiastic full stack software engineer with a background in, and passion for technology, medicine, and finance. I have
       strong skills in coding/programming, leadership, teamwork, communication, organization, self-motivation, and presentation - all 
       gained from firsthand experience. I am a seasoned professional with a diverse background in many fields, which enables me to quickly 
       adopt and master new technologies while working in both team and self-directed settings. I am always eager to continue learning and 

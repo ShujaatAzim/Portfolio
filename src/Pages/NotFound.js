6839301ca1207d34c7d7da78b66acd24f30cae40
Shujaat Azim <shujaat.azim@gmail.com>
2021-10-01 23:35:10 -0700

    Added back button to not found page.

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+
 
 const NotFound = () => {
+
+  let history = useHistory();
+
   return (
     <div className="notfound">
       <p style={{ margin: "0" }}>
         Oh no! The dreaded 404 error! Page not found :(
       </p>
+      {'\n'}
+      <div>
+        <Button onClick={() => history.goBack()}>Go Back</Button>
+      </div>
     </div>
   );
 }
